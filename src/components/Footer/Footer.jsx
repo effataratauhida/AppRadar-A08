3c334287309a47cf5ad4d9d37085cb0b7a72d84f
Effat Ara Tauhida <effataratauhida2020@gmail.com>
2025-10-08 18:23:44 +0600

    added banner section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import '../Footer/Footer.css'
 
 const Footer = () => {
     return (
         
-            <footer className='bg-[#001931]'>
+            <footer className=' bg-[#001931]'>
                 <div className='max-w-10/12 mx-auto py-8 flex flex-col md:flex-row  justify-between'>
                     <div>
                         <img src={logo} alt="" className='h-20 w-20 mb-1' />
                         <h2 className='font-bold text-3xl bg-gradient-to-r from-[#632EE3] to-[#9F62F2] 
                             bg-clip-text text-transparent'>AppRadar </h2>
                     </div>
-                    <div>
-                        <h5 className='text-gray-300 font-medium text-lg '>Services</h5>
+                    <div className='mt-3'>
+                        <h5>Services</h5>
                         <ul className='mt-3'>
-                            <li className='hover:underline  text-gray-300'><a href="">Branding</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Design</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Marketing</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Advertisement</a></li>
+                            <li ><a href="/" >Branding</a></li>
+                            <li><a href="/">Design</a></li>
+                            <li><a href="/">Marketing</a></li>
+                            <li><a href="/">Advertisement</a></li>
                             
                         </ul>
                     </div>
-                    <div>
-                        <h5 className='text-gray-300 font-medium text-lg '>Company</h5>
+                    <div className='mt-3'>
+                        <h5>Company</h5>
                         <ul className='mt-3'>
-                            <li className='hover:underline  text-gray-300'><a href="">About us</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Contact</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Jobs</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Press kit</a></li>
+                            <li><a href="/">About us</a></li>
+                            <li><a href="/">Contact</a></li>
+                            <li><a href="/">Jobs</a></li>
+                            <li><a href="/">Press kit</a></li>
                             
                         </ul>
                     </div>
-                    <div>
-                        <h5 className='text-gray-300 font-medium text-lg '>Legal</h5>
+                    <div className='mt-3'>
+                        <h5>Legal</h5>
                         <ul className='mt-3'>
-                            <li className='hover:underline  text-gray-300'><a href="">Terms of use</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Privacy policy</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Cookie policy</a></li>
-                            <li className='hover:underline text-gray-300'><a href="">Copyright Policy</a></li>
+                            <li><a href="/">Terms of use</a></li>
+                            <li><a href="/">Privacy policy</a></li>
+                            <li><a href="/">Cookie policy</a></li>
+                            <li><a href="/">Copyright Policy</a></li>
                             
                         </ul>
                     </div>
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import gitImg from '../../assets/git.png'
 import logo from '../../assets/logo.png'
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
+import  '../Header/Header.css'
 
 const Navbar = () => {
     return (
-        <div className='bg-white shadow-sm'>
+        <div className='bg-white'>
             <div className="navbar max-w-11/12 mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-black ">
                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"  fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                 </div>
-      <ul
+        <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <Link to='/' className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] 
-                            bg-clip-text text-transparent font-semibold text-base'>Home</Link>
-                        <Link to='/apps' className='text-base font-medium text-black'>Apps</Link>
-                        <Link to='/' className='text-base font-medium text-black'>Installation</Link>
-      </ul>
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li >
+                    <NavLink to='/' >Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/apps' >Apps</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/installation'>Installation</NavLink>
+                </li>
+        </ul>
                     </div>
-                    <Link to='/' className="flex items-center gap-1 hover:scale-105 ml-[-10px]">
+                    <Link to='/' className="flex items-center gap-1 hover:scale-105 ">
                         <img className='md:w-10 md:h-10 w-7 h-7' src={logo} alt="" />
                       <h2 className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] 
                       bg-clip-text text-transparent font-bold text-lg'>
@@ -29,19 +35,25 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="flex items-center gap-8">
-                        <Link to='/' className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] 
-                            bg-clip-text text-transparent font-semibold text-base'>Home</Link>
-                        <Link to='/apps' className='text-base font-medium text-black'>Apps</Link>
-                        <Link to='/' className='text-base font-medium text-black'>Installation</Link>
+                    <ul className="flex items-center gap-8 ">
+                        <li >
+                            <NavLink to='/' >Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/apps' >Apps</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/installation'>Installation</NavLink>
+                        </li>
+                        
                     </ul>
                 </div>
                 <div className="navbar-end">
-                          <button className='hover:scale-105 cursor-pointer rounded-sm bg-gradient-to-r
+                        <button className='hover:scale-105 cursor-pointer rounded-sm bg-gradient-to-r
                   from-[#632EE3] to-[#9F62F2] flex items-center gap-1.5 md:gap-2.5 py-2 px-3 md:py-3 md:px-4'>
                  <img src= {gitImg} alt="" />
                  <p className='text-white font-semibold text-base'>Contribute</p>
-            </button>
+                        </button>
                 </div>
             </div>
         </div>
